99e8d5d0a04456e1fe745d823ec400cac48a0189
Burnninel <79659103+Burnninel@users.noreply.github.com>
2021-12-20 16:44:11 -0300

    Finalizando o projeto

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,1 +1,12 @@
 import firebase from "firebase";
+import 'firebase/firestore'
+
+if(!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PRRJECT_ID,
+    })
+}
+
+export default firebase
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,20 @@
-import { useState } from "react";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
-import Cliente from "../core/Cliente";
+import useClientes from "../hooks/useClientes";
 
 export default function Home() {
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-
-  const clientes = [
-    new Cliente('Petuquio', 15, '1'),
-    new Cliente('Inrieta', 20, '2'),
-    new Cliente('Elary', 10, '3'),
-    new Cliente('Gigi', 13, '4')
-  ]
-
-  function clienteSelecionado(cliente: Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir... ${cliente.nome}`)
-  }
-
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
-    setVisivel('tabela')
-  }
-
-  function novoCliente() {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
+  const { 
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
 
   return (
     <div className={`
@@ -43,7 +23,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className="flex justify-end">
               <Botao cor="green" className="mb-4" onClick={novoCliente}>
@@ -51,15 +31,15 @@ export default function Home() {
               </Botao>
             </div>
             <Tabela clientes={clientes}
-              clienteSelecionado={clienteSelecionado}
-              clienteExcluido={clienteExcluido}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
             />
           </>
         ) : (
-          <Formulario 
+          <Formulario
             cliente={cliente}
             clientemudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')}
+            cancelado={exibirTabela}
           />
         )}
       </Layout>
